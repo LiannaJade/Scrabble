4f5fcdaa1e453613383f15741580d1e143033c09
Lianna <Lianna98701@gmail.com>
2024-01-17 14:47:30 +0000

    game setup complete, added temporary host to player communication, added DAWG and test file

--- scrabble.py
+++ scrabble.py
@@ -1,12 +1,42 @@
 import json
 import random
 import os
+import threading
+import time
 
 # TODO
 # game setup
 # player handling
 # game loop
 # game ending
+
+
+class DAWG:
+    """the basic idea of a DAWG, made to be more "python-friendly"
+    the saved structure is still stored as an automata"""
+    def __init__(self, strings):
+        strings.sort()
+        self.nodes = [{}]
+        for string in strings:
+            current_node = 0
+            for char, index in zip(string, range(len(string))):
+                if char in self.nodes[current_node].keys():
+                    current_node = self.nodes[current_node][char][0]
+                else:
+                    self.nodes[current_node][char] = (len(self.nodes), index == len(string)-1)
+                    current_node = len(self.nodes)
+                    self.nodes.append({})
+
+    def contains(self, string):
+        current_node = 0
+        for char, index in zip(string, range(len(string))):
+            if char in self.nodes[current_node].keys():
+                if index == len(string) - 1 and self.nodes[current_node][char][1]:
+                    return True
+                else:
+                    current_node = self.nodes[current_node][char][0]
+            else:
+                return False
 
 
 class TilePool:
@@ -56,15 +86,18 @@ class TilePool:
         return len(self.tiles)
 
 
-class Host:
+class Host(threading.Thread):
     def __init__(self, host_player, lang="en"):
-        self._started = False
+        super().__init__()
+        self.inputs = []
+        self._game_started = False
         self.players = []
         self.add_player(host_player)
         self.player_count = None
+        self.board = [[" " for j in range(15)] for i in range(15)]
         # gets tiles
         self.tiles = TilePool(lang)
-        # gets words
+        # gets words (possibly change to a GADDAG or DAWG)
         self.words = []
         if os.path.exists("Dictionaries/"+lang+".txt"):
             file = open("Dictionaries/"+lang+".txt", "r")
@@ -72,22 +105,22 @@ class Host:
             file.close()
         for i in range(len(self.words)):
             self.words[i] = self.words[i].replace("\n", "")
-        #
 
     def add_player(self, player):
         # add a player to the game
         # returns 1 if successful, else 0
         # this may need redoing when real multiplayer is made
-        if not self._started:
-            player_host = PlayerHost(player)
+        if not self._game_started:
+            player_host = PlayerHost(player, self)
             self.players.append(player_host)
+            player.host = player_host  # update this temp thing
             return True
         else:
             return False
 
     def start_game(self):
         # starting
-        self._started = True
+        self._game_started = True
         self.player_count = len(self.players)
         # draws tiles to decide order
         for i, j in zip(range(self.player_count), self.tiles.draw(self.player_count)):
@@ -95,18 +128,35 @@ class Host:
         self.players.sort(key=lambda n: n.tiles[0])
         for i in range(self.player_count):
             self.players[i].order = i
-            self.players[i].send("order {0}, tile: {1}".format(i, self.players[i].tiles.pop(0))) # change to actual command
+            self.players[i].send("order: {0}, order_tile: {1}".format(i, self.players[i].tiles.pop(0)))  # change to actual command
         # takes 7 tiles per player
         for player in self.players:
             player.tiles = self.tiles.take(7)
-            player.send("tiles: {0}".format(player.tiles))
+            player.send("tiles: {0}".format(player.tiles))  # change to actual command
         # start game loop
+        self.start()
+
+    def run(self):
+        self.game_loop()
+
+    def game_loop(self):
+        for player_index in range(self.player_count):
+            # tells all players the current player
+            for player in self.players:
+                player.send("current_player: {0}".format(player_index))
+            # waiting for response from player
+            waiting = True
+            while waiting:
+                if len(self.inputs) > 0:
+                    print("woooooo working {0}".format(self.inputs.pop(0)))
+                time.sleep(1)
 
 
 class PlayerHost:
     # class used by the host to handle players
-    def __init__(self, player):
+    def __init__(self, player, host):
         self.player = player
+        self.host = host
         self.tiles = []
         self.order = None
 
@@ -114,14 +164,40 @@ class PlayerHost:
         # update to actual code
         self.player.receive(command)
 
+    def receive(self, string):
+        self.host.inputs.append(string)
+
 
 class PlayerClient:
     def __init__(self, name):
         self.name = name
+        self.order = None
+        self.board = None
+        self.tiles = None
+        self.player_count = None
+        self.player_turn = None
+
+        self.host = None  # temp variable until implementation
 
     def receive(self, command):
         # update to actual code
+        try:
+            commands = command.split(",")
+            for command in commands:
+                opcode, operand = command.split(":")
+                if opcode == "order":
+                    self.order = int(operand)
+                elif opcode == "order_tile":
+                    print(operand)
+                elif opcode == "tiles":
+                    self.tiles = operand
+        except:
+            pass
         print(self.name, command)
+
+    def send(self, string):
+        # update to actual code
+        self.host.receive(string)
 
 
 if __name__ == "__main__":
@@ -130,3 +206,8 @@ if __name__ == "__main__":
     player2 = PlayerClient("player 2")
     host.add_player(player2)
     host.start_game()
+
+    time.sleep(2)
+
+    while True:
+        exec(input("->"))
---
+++ tests.py
@@ -0,0 +1,123 @@
+import scrabble
+import random
+
+
+def dawg_test(error=None):
+    # creates a random set of strings for testing purposes
+    strings1 = []  # set of strings with no repeats, or substrings
+    strings2 = []  # set of strings containing strings from strings1
+    strings3 = []  # set if strings contains duplicates from strings1
+    letters = [chr(i) for i in range(ord("A"), ord("Z") + 1)]
+    # creates 8.000 random strings
+    counter = 0
+    while counter < 8000:
+        string = ""
+        # generates string between 2 and 15 characters
+        for j in range(random.randint(2, 15)):
+            string += random.choice(letters)
+        # checks if string is a subset of any other word in strings1
+        if string not in [i[:len(string)] for i in strings1]:
+            strings1.append(string)
+            counter += 1
+    print("strings1: created")
+    # adds 0.000 substrings to strings2, for a total of 100.000 strings (some will be repeats)
+    counter = 0
+    while counter < 1000:
+        string = random.choice(strings1)
+        if len(string) != 2:
+            new_string = string[:random.randint(2, len(string) - 1)]
+            if new_string not in strings1 and new_string not in strings2:
+                strings2.append(new_string)
+                counter += 1
+    print("strings2: created")
+    # adds 1.000 duplicates to strings3
+    counter = 0
+    print("strings3: created")
+    while counter < 1000:
+        strings3.append(random.choice(strings1))
+        counter += 1
+    # tests1 DAWG creation
+    a, b, c, d = [None]*4
+    try:
+        a = scrabble.DAWG(strings1)
+        print("DAWG a: created")
+    except error:
+        print("DAWG a: failed", error)
+    try:
+        b = scrabble.DAWG(strings1+strings2)
+        print("DAWG b: created")
+    except error:
+        print("DAWG b: failed", error)
+    try:
+        c = scrabble.DAWG(strings1+strings3)
+        print("DAWG c: created")
+    except error:
+        print("DAWG c: failed", error)
+    try:
+        d = scrabble.DAWG(strings1+strings2+strings3)
+        print("DAWG d: created")
+    except error:
+        print("DAWG d: failed", error)
+    DAWGs = []
+    for key, value in locals().items():
+        if isinstance(value, scrabble.DAWG):
+            DAWGs.append([key, value])
+    # test2 retrieval
+    for name, dawg in DAWGs:
+        print("Retrieval tests for dawg {0}".format(name))
+        try:
+            true, false, none, = [0] * 3
+            for string in strings1:
+                rtn = dawg.contains(string)
+                if rtn:
+                    true += 1
+                elif not rtn:
+                    false += 1
+                else:
+                    none += 1
+            print("test 1: found {0}, not found {1}, none {2}".format(true, false, none))
+        except error:
+            print("test 1: failed", error)
+        try:
+            true, false, none, = [0] * 3
+            for string in strings1 + strings2:
+                rtn = dawg.contains(string)
+                if rtn:
+                    true += 1
+                elif not rtn:
+                    false += 1
+                else:
+                    none += 1
+            print("test 2: found {0}, not found {1}, none {2}".format(true, false, none))
+        except error:
+            print("test 2: failed", error)
+        try:
+            true, false, none, = [0] * 3
+            for string in strings1 + strings3:
+                rtn = dawg.contains(string)
+                if rtn:
+                    true += 1
+                elif not rtn:
+                    false += 1
+                else:
+                    none += 1
+            print("test 3: found {0}, not found {1}, none {2}".format(true, false, none))
+        except error:
+            print("test 3: failed", error)
+        try:
+            true, false, none, = [0] * 3
+            for string in strings1 + strings2 + strings3:
+                rtn = dawg.contains(string)
+                if rtn:
+                    true += 1
+                elif not rtn:
+                    false += 1
+                else:
+                    none += 1
+            print("test 4: found {0}, not found {1}, none {2}".format(true, false, none))
+        except error:
+            print("test 4: failed", error)
+
+
+if __name__ == "__main__":
+    dawg_test()
